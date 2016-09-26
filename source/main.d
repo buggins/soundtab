19e9abd9de421b2a9a503fbc76fa8e1488cf8102
Vadim Lopatin <coolreader.org@gmail.com>
2016-09-26 16:01:31 +0300

    more wintab functions added

--- source/main.d
+++ source/main.d
@@ -8,22 +8,26 @@ mixin APP_ENTRY_POINT;
 extern (C) int UIAppMain(string[] args) {
 
     import derelict.wintab.wintab;
-    //import core.sys.windows.windows;
+    import core.sys.windows.windows;
     DerelictWintab.load();
 
     uint res;
-
-    ushort thisVersion;
-    res = WTInfo(WTI_INTERFACE, IFC_SPECVERSION, &thisVersion);
-
-    LOGCONTEXT	glogContext;
-	uint wWTInfoRetVal = WTInfo(WTI_DEFSYSCTX, 0, &glogContext);
 
     if (!WTInfo(0, 0, null)) {
         Log.e("WinTab services not available");
         //return 1;
     }
-    char[50] wname;
+
+    ushort thisVersion;
+    res = WTInfo(WTI_INTERFACE, IFC_SPECVERSION, &thisVersion);
+
+    LOGCONTEXT	glogContext;
+	glogContext.lcOptions |= CXO_SYSTEM;
+	uint wWTInfoRetVal = WTInfo(WTI_DEFSYSCTX, 0, &glogContext);
+    assert(glogContext.lcOptions & CXO_SYSTEM);
+    assert(wWTInfoRetVal == glogContext.sizeof);
+
+    wchar[50] wname;
     if (!WTInfo(WTI_DEVICES, DVC_NAME, cast(void*)wname.ptr)) {
         Log.e("WinTab cannot get device name");
         return 1;
@@ -37,6 +41,62 @@ extern (C) int UIAppMain(string[] args) {
 	uint tilt_support = WTInfo(WTI_DEVICES, DVC_ORIENTATION, cast(void*)&tpOri);
     // load theme from file "theme_default.xml"
     //Platform.instance.uiTheme = "theme_default";
+
+    immutable uint PACKETDATA = (PK_X | PK_Y | PK_BUTTONS | PK_NORMAL_PRESSURE);
+    immutable uint PACKETMODE = PK_BUTTONS;
+
+	// What data items we want to be included in the tablet packets
+	glogContext.lcPktData = PACKETDATA;
+
+	// Which packet items should show change in value since the last
+	// packet (referred to as 'relative' data) and which items
+	// should be 'absolute'.
+	glogContext.lcPktMode = PACKETMODE;
+
+	// This bitfield determines whether or not this context will receive
+	// a packet when a value for each packet field changes.  This is not
+	// supported by the Intuos Wintab.  Your context will always receive
+	// packets, even if there has been no change in the data.
+	glogContext.lcMoveMask = PACKETDATA;
+
+	// Which buttons events will be handled by this context.  lcBtnMask
+	// is a bitfield with one bit per button.
+	glogContext.lcBtnUpMask = glogContext.lcBtnDnMask;
+
+	AXIS TabletX;
+	AXIS TabletY;
+	// Set the entire tablet as active
+	wWTInfoRetVal = WTInfo( WTI_DEVICES + 0, DVC_X, &TabletX );
+    assert(wWTInfoRetVal == AXIS.sizeof);
+	wWTInfoRetVal = WTInfo( WTI_DEVICES, DVC_Y, &TabletY );
+    assert(wWTInfoRetVal == AXIS.sizeof);
+
+	glogContext.lcInOrgX = 0;
+	glogContext.lcInOrgY = 0;
+	glogContext.lcInExtX = TabletX.axMax;
+	glogContext.lcInExtY = TabletY.axMax;
+
+	glogContext.lcInOrgX = 0;
+	glogContext.lcInOrgY = 0;
+	glogContext.lcInExtX = TabletX.axMax;
+	glogContext.lcInExtY = TabletY.axMax;
+
+	// Guarantee the output coordinate space to be in screen coordinates.  
+	glogContext.lcOutOrgX = GetSystemMetrics( SM_XVIRTUALSCREEN );
+	glogContext.lcOutOrgY = GetSystemMetrics( SM_YVIRTUALSCREEN );
+	glogContext.lcOutExtX = GetSystemMetrics( SM_CXVIRTUALSCREEN ); //SM_CXSCREEN );
+
+	// In Wintab, the tablet origin is lower left.  Move origin to upper left
+	// so that it coincides with screen origin.
+	glogContext.lcOutExtY = -GetSystemMetrics( SM_CYVIRTUALSCREEN );	//SM_CYSCREEN );
+
+	// Leave the system origin and extents as received:
+	// lcSysOrgX, lcSysOrgY, lcSysExtX, lcSysExtY
+
+	// open the region
+	// The Wintab spec says we must open the context disabled if we are 
+	// using cursor masks.  
+	//hctx = gpWTOpenA( hWnd, &glogContext, FALSE );
 
     // create window
     Log.d("Creating window");
