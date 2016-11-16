3a8167277e1f3f646efaeee422eb4093ef39ab72
Vadim Lopatin <coolreader.org@gmail.com>
2016-11-16 16:23:38 +0300

    phoneme instr fixed

--- source/soundtab/audio/instruments.d
+++ source/soundtab/audio/instruments.d
@@ -242,37 +242,6 @@ class InstrumentBaseF : Instrument {
 
 }
 
-/// integer calculations instrument base class
-class InstrumentBase : Instrument {
-    protected double _currentPitch = 0; // Hz
-    protected double _currentGain = 0; // 0..1
-    protected double _currentController1 = 0; // 0..1
-
-    protected int _phase_mul_256 = 0;
-
-    protected int _target_step_mul_256 = 0; // step*256 inside wavetable to generate requested frequency
-    protected int _target_gain_mul_65536 = 0;
-    protected int _target_controller1_mul_65536 = 0;
-    protected int _step_mul_256 = 0; // step*256 inside wavetable to generate requested frequency
-    protected int _gain_mul_65536 = 0;
-    protected int _controller1_mul_65536 = 0;
-
-    protected override void calcParams() {
-        // copy dynamic values
-        _currentPitch = _targetPitch;
-        _currentGain = _targetGain;
-        //_currentController1 = _targetController1;
-        double onePeriodSamples = samplesPerSecond / _currentPitch;
-        double step = WAVETABLE_SIZE / onePeriodSamples;
-        _target_step_mul_256 = cast(int)(step * 256);
-        _target_gain_mul_65536 = cast(int)(_currentGain * 0x10000);
-        _target_controller1_mul_65536 = cast(int)(_currentController1 * 0x10000);
-    }
-
-    override protected void onFormatChanged() {
-        _phase_mul_256 = 0;
-    }
-}
 
 void limitDistortion(ref float v) {
     if (v < -0.9f) {
@@ -509,8 +478,8 @@ class PhonemeOscillerF : OscillerF {
     OnePole _onepole;
     Noise _noise;
     this(PhonemeType phoneme, float formantFreqMult = 1) {
-        super(SAW_TABLE_F, 1, 0, mulSequence(1, -0.3, 20).dup);//VOICE_IMPULSE_F SAW_TABLE_F VOICE_EXPERIMENTAL_F
-        //super(VOICE_EX_1_F);//VOICE_IMPULSE_F SAW_TABLE_F VOICE_EXPERIMENTAL_F VOICE_EX_1_F
+        super(SIN_TABLE_F, 1, 0, mulSequence(1, -0.85, 40).dup);//VOICE_IMPULSE_F SAW_TABLE_F VOICE_EXPERIMENTAL_F
+        //super(VOICE_EXPERIMENTAL_F);//VOICE_IMPULSE_F SAW_TABLE_F VOICE_EXPERIMENTAL_F VOICE_EX_1_F
         _formantFilter = new PhonemeFormantsFilter(phoneme, formantFreqMult);
         _onezero = new OneZero();
         _onepole = new OnePole();
@@ -530,10 +499,10 @@ class PhonemeOscillerF : OscillerF {
         //    value = 1;
         //else
         //    value = 0;
-        if ((oldPhase >> 8) < WAVETABLE_SIZE / 12) {
-            if (noiseGain > 0)
-                value += _noise.tick() * noiseGain;
-        }
+        //if ((oldPhase >> 8) < WAVETABLE_SIZE / 12) {
+        //    if (noiseGain > 0)
+        //        value += _noise.tick() * noiseGain;
+        //}
 
         value = _onepole.tick( _onezero.tick( value ) );
 
@@ -748,7 +717,7 @@ class PhonemeSynth : InstrumentBaseF {
         Controller[] res;
         res ~= Controller(ControllerId.VibratoAmount, "Vibrato Amount", 0, 1000, 300);
         res ~= Controller(ControllerId.VibratoFreq, "Vibrato Freq", 0, 1000, 500);
-        res ~= Controller(ControllerId.Chorus, "Chorus", 0, 1000, 0);
+        res ~= Controller(ControllerId.Chorus, "Chorus", 0, 1000, 1000);
         //res ~= Controller(ControllerId.Noise, "Noise", 0, 1000, 0);
         res ~= Controller(ControllerId.Distortion, "Distortion", 0, 1000, 0);
         return cast(immutable(Controller)[])res;
@@ -761,194 +730,6 @@ class PhonemeSynth : InstrumentBaseF {
 
 }
 
-class MyAudioSource : InstrumentBase {
-
-
-
-    int[] _wavetable;
-
-
-    Osciller _vibrato0;
-    Osciller _vibrato1;
-    Osciller _vibrato2;
-    Osciller _vibrato3;
-    Osciller _vibrato4;
-    Osciller _vibrato5;
-    Osciller _vibrato21;
-    Osciller _vibrato22;
-    Osciller _vibrato23;
-    Osciller _vibrato24;
-    Osciller _vibrato25;
-    Osciller _tone1;
-    Osciller _tone2;
-    Osciller _tone3;
-    Osciller _tone4;
-    Osciller _tone5;
-    Osciller _tone21;
-    Osciller _tone22;
-    Osciller _tone23;
-    Osciller _tone24;
-    Osciller _tone25;
-
-    this() {
-        _id = "ethereal";
-        _name = "Ethereal";
-        _wavetable = SIN_TABLE; //genWaveTableSquare(); //genWaveTableSin();
-        //_wavetable = genWaveTableSquare(); //genWaveTableSin();
-        _vibrato0 = new Osciller(SIN_TABLE, 500, 0x10000);
-        _vibrato1 = new Osciller(SIN_TABLE, 500, 0x10000);
-        _vibrato2 = new Osciller(SIN_TABLE, 600, 0x10000);
-        _vibrato3 = new Osciller(SIN_TABLE, 700, 0x10000);
-        _vibrato4 = new Osciller(SIN_TABLE, 1000, 0x10000);
-        _vibrato5 = new Osciller(SIN_TABLE, 400, 0x10000);
-        _vibrato21 = new Osciller(SIN_TABLE, 700, 0x10000);
-        _vibrato22 = new Osciller(SIN_TABLE, 800, 0x10000);
-        _vibrato23 = new Osciller(SIN_TABLE, 900, 0x10000);
-        _vibrato24 = new Osciller(SIN_TABLE, 700, 0x10000);
-        _vibrato25 = new Osciller(SIN_TABLE, 500, 0x10000);
-        _tone1 = new Osciller(_wavetable);
-        _tone2 = new Osciller(_wavetable);
-        _tone3 = new Osciller(_wavetable);
-        _tone4 = new Osciller(_wavetable);
-        _tone5 = new Osciller(_wavetable);
-        _tone21 = new Osciller(_wavetable);
-        _tone22 = new Osciller(_wavetable);
-        _tone23 = new Osciller(_wavetable);
-        _tone24 = new Osciller(_wavetable);
-        _tone25 = new Osciller(_wavetable);
-    }
-
-    override protected void onFormatChanged() {
-        _phase_mul_256 = 0;
-        _vibrato0.setPitch(2.234, samplesPerSecond);
-        _vibrato1.setPitch(2.234, samplesPerSecond);
-        _vibrato2.setPitch(2.12367, samplesPerSecond);
-        _vibrato3.setPitch(2.37615263, samplesPerSecond);
-        _vibrato4.setPitch(2.78431, samplesPerSecond);
-        _vibrato5.setPitch(2.34562, samplesPerSecond);
-        _vibrato21.setPitch(2.65321, samplesPerSecond);
-        _vibrato22.setPitch(2.5432, samplesPerSecond);
-        _vibrato23.setPitch(2.4321, samplesPerSecond);
-        _vibrato24.setPitch(2.36345, samplesPerSecond);
-        _vibrato25.setPitch(2.76434, samplesPerSecond);
-    }
-
-    //int durationCounter = 100;
-
-    override bool loadData(int frameCount, ubyte * buf, ref uint flags) {
-        {
-            lock();
-            scope(exit)unlock();
-            calcParams();
-        }
-
-        int frameMillis = frameCount < 10 ? 10 : 1000 * frameCount / samplesPerSecond;
-
-        int lastGain = _gain_mul_65536;
-        int lastController1 = _controller1_mul_65536;
-
-        _step_mul_256 = _target_step_mul_256;
-        _controller1_mul_65536 = _target_controller1_mul_65536;
-
-        if (_gain_mul_65536 < _target_gain_mul_65536) {
-            // attack
-            if (frameMillis > _attack)
-                _gain_mul_65536 = _target_gain_mul_65536;
-            else {
-                _gain_mul_65536 = _gain_mul_65536 + (_target_gain_mul_65536 - _gain_mul_65536) * frameMillis / _attack;
-            }
-        } else {
-            // release
-            if (frameMillis > _release)
-                _gain_mul_65536 = _target_gain_mul_65536;
-            else {
-                _gain_mul_65536 = _gain_mul_65536 + (_target_gain_mul_65536 - _gain_mul_65536) * frameMillis / _release;
-            }
-        }
-
-        Log.d("Instrument loadData frameCount=", frameCount, " lastGain=", lastGain, " newGain=", _gain_mul_65536);
-
-        for (int i = 0; i < frameCount; i++) {
-            /// one step
-            int gain = interpolate(i, frameCount, lastGain, _gain_mul_65536);
-            int controller1 = interpolate(i, frameCount, lastController1, _controller1_mul_65536);
-
-            int vibratoAmount0 = (500 * controller1) >> 16;
-            int vibratoAmount1 = (500 * controller1) >> 16;
-            int vibratoAmount2 = (700 * controller1) >> 16;
-            int vibratoAmount3 = (600 * controller1) >> 16;
-            int vibratoAmount4 = (400 * controller1) >> 16;
-            int vibratoAmount5 = (300 * controller1) >> 16;
-            //vibratoAmount1 = vibratoAmount2 = vibratoAmount3 = vibratoAmount4 = vibratoAmount5 = 0;
-
-
-            //int gain_vibrato = _vibrato4.step();
-
-            int gain1 = gain; //cast(int)((cast(long)gain * gain_vibrato) >> 16); // left
-            int gain2 = gain; //cast(int)((cast(long)gain * (0x20000 - gain_vibrato)) >> 16); // right
-
-            //int step = _step_mul_256; //_vibrato0.stepMultiply(_step_mul_256, vibratoAmount1);
-            int step = _vibrato0.stepMultiply(_step_mul_256, vibratoAmount1);
-
-            int wt_value1 = _tone1.stepWithGain(_vibrato1.stepMultiply(step, vibratoAmount1), 20000);
-            int wt_value2 = _tone2.stepWithGain(_vibrato2.stepMultiply(step * 2, vibratoAmount2), 10000);
-            int wt_value3 = _tone3.stepWithGain(_vibrato3.stepMultiply(step * 3, vibratoAmount3), 7000);
-            int wt_value4 = _tone4.stepWithGain(_vibrato4.stepMultiply(step * 4, vibratoAmount4), 4000);
-            int wt_value5 = _tone5.stepWithGain(_vibrato4.stepMultiply(step * 5, vibratoAmount5), 2000);
-
-            int wt_value21 = _tone1.stepWithGain(_vibrato21.stepMultiply(step, vibratoAmount1), 20000);
-            int wt_value22 = _tone2.stepWithGain(_vibrato22.stepMultiply(step * 2, vibratoAmount2), 10000);
-            int wt_value23 = _tone3.stepWithGain(_vibrato23.stepMultiply(step * 3, vibratoAmount3), 7000);
-            int wt_value24 = _tone4.stepWithGain(_vibrato24.stepMultiply(step * 4, vibratoAmount4), 3000);
-            int wt_value25 = _tone4.stepWithGain(_vibrato24.stepMultiply(step * 5, vibratoAmount5), 2000);
-
-
-            //int wt_value_1 = wt_value1;
-            //int wt_value_2 = wt_value2;
-
-            //int inv_controller1 = 0x10000 - controller1;
-            int wt_value_1 = wt_value1 + wt_value2 + wt_value3 + wt_value4 + wt_value5;
-            int wt_value_2 = wt_value21 + wt_value22 + wt_value23 + wt_value24 + wt_value25;
-
-            int sample1 = (wt_value_1 * gain1) >> 16;
-            int sample2 = (wt_value_2 * gain2) >> 16;
-
-            if (sample1 < -32767)
-                sample1 = -32767;
-            else if (sample1 > 32767)
-                sample1 = 32767;
-            if (sample2 < -32767)
-                sample2 = -32767;
-            else if (sample2 > 32767)
-                sample2 = 32767;
-            putSamples(buf, sample1, sample2);
-
-            buf += blockAlign;
-        }
-
-        // TODO
-        //durationCounter--;
-        flags = 0; //durationCounter <= 0 ? AUDCLNT_BUFFERFLAGS.AUDCLNT_BUFFERFLAGS_SILENT : 0;
-        //Log.d("Instrument loadData - exit");
-        return true;
-    }
-
-    /// returns list of supported controllers
-    override immutable(Controller)[] getControllers() {
-        Controller[] res;
-        res ~= Controller(ControllerId.Chorus, "Chorus", 0, 1000, 300);
-        res ~= Controller(ControllerId.Reverb, "Reverb", 0, 1000, 300);
-        res ~= Controller(ControllerId.VibratoAmount, "Vibrato Amount", 0, 1000, 300);
-        res ~= Controller(ControllerId.VibratoFreq, "Vibrato Freq", 0, 1000, 500);
-        return cast(immutable(Controller)[])res;
-    }
-
-    /// returns true if controller value is set, false for unknown controller
-    override bool updateController(ControllerId id, int value) {
-        return super.updateController(id, value);
-    }
-
-}
 
 private __gshared Instrument[] _instrumentList;
 /// get list of supported instruments
@@ -970,7 +751,6 @@ Instrument[] getInstrumentList() {
         _instrumentList ~= new PhonemeSynth("voiceOoo", "Voice Ooo", PhonemeType.ooo);
         _instrumentList ~= new PhonemeSynth("voiceRrr", "Voice Rrr", PhonemeType.rrr);
         _instrumentList ~= new PhonemeSynth("voiceLll", "Voice Lll", PhonemeType.lll);
-        _instrumentList ~= new MyAudioSource();
     }
     return _instrumentList;
 }
